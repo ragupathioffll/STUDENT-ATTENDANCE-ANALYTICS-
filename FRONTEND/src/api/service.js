6aed9f19f3f0dc8e23f4ccc1db6e1fffdc0b7efe
ragupathi <ragumurugesan172@gmail.com>
2026-04-06 10:12:25 +0530

    update in sever

--- FRONTEND/src/api/service.js
+++ FRONTEND/src/api/service.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'https://student-attendance-analytics-1.onrender.com/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://student-attendance-analytics-2.onrender.com/api';
 
 const getHeaders = () => {
     const token = localStorage.getItem('token') || sessionStorage.getItem('token');
